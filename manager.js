ce716bfc634ce30c17eaf3852a25861eb7651d07
esketchandu <esketchandu10@gmail.com>
2024-04-27 14:28:07 -0700

    phase 04 solved

--- manager.js
+++ manager.js
@@ -9,9 +9,30 @@ class Manager extends Employee {
   addEmployee(employee){
   if (employee instanceof Employee) {
     this.employees.push(employee);
+    }
   }
+
+  calculateBonus(multiplier) {
+    return (this.salary + this._totalSubSalary()) * multiplier;
   }
+
+  _totalSubSalary() {
+    let sum = 0;
+    for(const employee of this.employees) {
+      if (employee instanceof Manager) {
+        sum += employee.salary + employee._totalSubSalary();
+      }
+      else {
+        sum += employee.salary
+      }
+    }
+    return sum;
+  }
+
 }
+
+
+
 
 
 // const splinter = new Manager('Splinter', 100000, 'Sensai');
@@ -29,14 +50,24 @@ class Manager extends Employee {
 
 // console.log('After: ', splinter);
 
+// const splinter = new Manager('Splinter', 100000, 'Sensei');
+// console.log('Before: ', splinter);
+
+// const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+// const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+// const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+// const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+// console.log('After: ', splinter);
+
 const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log('Before: ', splinter);
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
 
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
-
-console.log('After: ', splinter);
+console.log(splinter.calculateBonus(0.05)); // => 22500
+console.log(leo.calculateBonus(0.05)); // => 17500
+console.log(raph.calculateBonus(0.05)); // => 13000
 
 module.exports = Manager;
